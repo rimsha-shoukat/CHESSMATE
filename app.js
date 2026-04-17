bafcc1ab16d6c794cd23abafea68295700ff1b63
rimsha-shoukat <rimsha.devs@gmail.com>
2026-04-17 18:27:45 +0500

    mobile first approach added (HTML + CSS)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index");
