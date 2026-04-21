4edd518395f7eb0b543075d8f37ef185e7c6eb45
rimsha-shoukat <rimsha.devs@gmail.com>
2026-04-21 20:44:43 +0500

    bot upgraded, make bot best move (stockfish)

--- public/scripts/bot.js
+++ public/scripts/bot.js
@@ -1,40 +1,45 @@
+const stockfish = new Worker("/scripts/stockfish.js");
+stockfish.postMessage("uci");
+
+stockfish.onmessage = function (e) {
+    if (!e.data.startsWith("bestmove")) return;
+    console.log(e.data)
+    const best = e.data.split(" ")[1];
+    const moved = chess.move({ from: best.slice(0, 2), to: best.slice(2, 4), promotion: "q" });
+    if (!moved) return;
+    if (moved.captured) player.innerText += " " + getPieceUnicode({ type: moved.captured });
+
+    updateUI();
+    renderBoard();
+    checkGameOver();
+};
+
+function makeBotMove() {
+    if (chess.game_over()) return;
+    stockfish.postMessage("position fen " + chess.fen());
+    stockfish.postMessage("go depth 10");
+}
+
 function handleDrop(e) {
     e.preventDefault();
     if (!dragPiece) return;
+
     const targetSquare = { row: parseInt(e.currentTarget.dataset.row), col: parseInt(e.currentTarget.dataset.col) };
     const isValidMove = handleMove(sourceSquare, targetSquare);
+
     if (isValidMove) {
-        const capturedPieceSymbol = getPieceUnicode({ type: isValidMove.captured });
         if (isValidMove.captured) {
-            if (chess.turn() === 'b') {
-                you.innerText += " " + capturedPieceSymbol;
-            } else {
-                player.innerText += " " + capturedPieceSymbol;
-            }
+            if (chess.turn() === 'b') you.innerText += " " + getPieceUnicode({ type: isValidMove.captured });
+            else player.innerText += " " + getPieceUnicode({ type: isValidMove.captured });
         }
         updateUI();
         renderBoard();
         checkGameOver();
-        if (!chess.game_over()) {
-            setTimeout(makeBotMove, 500);
-        }
+        if (!chess.game_over()) setTimeout(makeBotMove, 1000);
     }
+
     dragPiece = null;
     sourceSquare = null;
-}
-
-function makeBotMove() {
-    const possibleMoves = chess.moves();
-    if (chess.game_over() || possibleMoves.length == 0) return;
-    const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-    const moved = chess.move(move);
-    if (moved.captured) {
-        const capturedPieceSymbol = getPieceUnicode({ type: moved.captured });
-        player.innerText += " " + capturedPieceSymbol;
-    }
-    updateUI();
-    renderBoard();
-    checkGameOver();
 }
 
 updateUI();
